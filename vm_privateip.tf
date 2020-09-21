361073f7af1ba05c52fd06a256be0f133b16afee
Niharika Thimmareddy <niharika.bhoomi15@gmail.com>
2020-09-21 18:02:45 -0400

    11

--- vm_privateip.tf
+++ vm_privateip.tf
@@ -7,12 +7,7 @@ resource "azurerm_resource_group" "myterraformgroup1" {
     }
 }
 
-resource "azurerm_subnet" "myterraformsubnet1" {
-    name                 = "mySubnet1"
-    resource_group_name  = azurerm_resource_group.myterraformgroup1.name
-    virtual_network_name = azurerm_virtual_network.myterraformnetwork1.name
-    address_prefixes       = ["10.1.0.0/24"]
-}
+
 
 resource "azurerm_virtual_network" "myterraformnetwork1" {
     name                = "myVnet1"
@@ -25,7 +20,12 @@ resource "azurerm_virtual_network" "myterraformnetwork1" {
     }
 }
 
-
+resource "azurerm_subnet" "myterraformsubnet1" {
+    name                 = "mySubnet1"
+    resource_group_name  = azurerm_resource_group.myterraformgroup1.name
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork1.name
+    address_prefixes       = ["10.1.0.0/24"]
+}
 
 
 resource "azurerm_network_security_group" "myterraformnsg1" {
