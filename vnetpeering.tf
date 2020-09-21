175a24eade2e080fb6493fd7531aa345232e8819
Niharika Thimmareddy <niharika.bhoomi15@gmail.com>
2020-09-21 17:40:25 -0400

    updated

--- vnetpeering.tf
+++ vnetpeering.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network_peering" "example-1" {
   name                      = "peer1to2"
-  resource_group_name       = azurerm_resource_group.myterraformgroup1.name
+  resource_group_name       = azurerm_resource_group.myterraformgroup.name
   virtual_network_name      = azurerm_virtual_network.myterraformnetwork.name
   remote_virtual_network_id = azurerm_virtual_network.myterraformnetwork1.id
 }
