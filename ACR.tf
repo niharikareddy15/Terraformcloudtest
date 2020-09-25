3e3e57ce13bbde2dfcab84611da0c575fa0ae86d
Niharika Thimmareddy <niharika.bhoomi15@gmail.com>
2020-09-25 16:24:23 -0400

    creating registry

---
+++ ACR.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "resourceGroupACR"
+  location = "East US"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                     = "containerRegistry1"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  sku                      = "Standard"
+  admin_enabled            = false
+}
