8ae063323412f51fb63fc1581b8bed2e2968e73b
Niharika Thimmareddy <niharika.bhoomi15@gmail.com>
2020-09-30 18:43:46 -0400

    pod identity

---
+++ pod_identity.tf
@@ -0,0 +1,9 @@
+
+resource "azurerm_user_assigned_identity" "testIdentity" {
+  resource_group_name = azurerm_resource_group.myterraformgroup2.name
+  location            = azurerm_resource_group.myterraformgroup2.location
+
+  name = "identity1"
+
+  tags = var.tags
+}
