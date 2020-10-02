55f28d83c7e6a5e518fb5bfe6225a435c2cd7c01
Akhil M <akhildevops24@gmail.com>
2020-10-02 16:14:47 -0400

    Delete output.tf

--- output.tf
+++
@@ -1,7 +0,0 @@
-/*output "identity_resource_id" {
-    value = azurerm_user_assigned_identity.testIdentity.id
-}
-
-output "identity_client_id" {
-    value = azurerm_user_assigned_identity.testIdentity.client_id
-}
